dba0455d013edadf02b75a518c97ccd432811438
Cammeyong <cammeyon@gmail.com>
2023-04-28 06:37:35 -0500

    added error message to mongoose config

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,6 +4,6 @@ mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://127.0.0.1:27017/taskManagerdb')
 .then(()=>{console.log('connected to db')})
-.catch((error)=>{console.log(error)});
+.catch(( error)=>{console.log('Error occured while connecting to db', error)});
 
 module.exports = mongoose;
